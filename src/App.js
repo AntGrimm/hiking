9d537a1ea55bd2440761fabb07b1c34d19f148d5
Tony Grimm <anthony.l.grimm@gmail.com>
2019-08-27 12:27:54 -0400

    Finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import CurrentHikeLocation from './components/CurrentHikeLocation'
+import MilesHiked from './components/MilesHiked'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <header>
+          <h1>My Current Hike!</h1>
+          <CurrentHikeLocation />
+          <MilesHiked />
+        </header>
+      </>
+    )
   }
 }
 
